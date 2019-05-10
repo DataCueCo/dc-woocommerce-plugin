1d22fad17b849b78fa7b006e33ab97d95cdfe424
chenruimin <ruiminch@163.com>
2019-05-10 20:52:36 +0800

    sent DELETE /v1/clients when plugin is deleted

--- uninstall.php
+++ uninstall.php
@@ -7,3 +7,21 @@ wp_clear_scheduled_hook( 'datacue_worker_cron' );
 global $wpdb;
 $sql = "DROP TABLE IF EXISTS {$wpdb->prefix}datacue_queue;";
 $wpdb->query($sql);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// clear client
+$env = file_exists(__DIR__ . '/staging') ? 'development' : 'production'; // development or production
+$dataCueOptions = get_option('datacue_options');
+if ($dataCueOptions) {
+    try {
+        $client = new \DataCue\Client(
+            $dataCueOptions['api_key'],
+            $dataCueOptions['api_secret'],
+            ['max_try_times' => 3],
+            $env
+        );
+        $client->client->clear();
+    } catch (\Exception $e) {
+    }
+}
